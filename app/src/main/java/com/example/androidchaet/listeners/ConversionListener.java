c66b5a396e07143597c9c42294fb7bff0d93265a
andrea <a.neira@pucp.edu.pe>
2024-07-15 22:43:46 -0500

    shat

---
+++ app/src/main/java/com/example/androidchaet/listeners/ConversionListener.java
@@ -0,0 +1,6 @@
+package com.example.androidchaet.listeners;
+import com.example.androidchaet.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/androidchaet/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.androidchaet.listeners;
+
+import com.example.androidchaet.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
